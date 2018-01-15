4570894c7a4ef94b2c8178c74b1d21495b2eea5a
Bhaavan Merchant <bhaavanmerchant@gmail.com>
2018-01-15 15:19:39 +0000

    Do not tokenize on '\n'

--- data/generate_training_data.py
+++ data/generate_training_data.py
@@ -8,7 +8,8 @@ from numpy.random import shuffle as random_shuffle
 from numpy.random import rand
 
 input_filename = '/var/data/correct_en_snt.txt'
-output_filename = '/tmp/mistakes_en_snt.txt'
+output_correct_filename = '/tmp/correct_en_snt.txt'
+output_mistake_filename = '/tmp/mistakes_en_snt.txt'
 
 # Parameters for the model and dataset
 MAX_INPUT_LEN = 40
@@ -31,7 +32,7 @@ def add_noise_to_string(a_string, amount_of_noise):
             # Add a random character
             random_char_position = random_randint(len(a_string))
             a_string = a_string[:random_char_position] + random_choice(CHARS[:-1]) + a_string[random_char_position:]
-        if rand() < amount_of_noise * len(a_string):
+        if (rand() < amount_of_noise * len(a_string)) and len(a_string) > 3:
             # Transpose 2 characters
             random_char_position = random_randint(len(a_string) - 1)
             a_string = (a_string[:random_char_position] +
@@ -41,8 +42,11 @@ def add_noise_to_string(a_string, amount_of_noise):
 	return a_string
 
 with open(input_filename, 'r') as i_f:
-    with open(output_filename, 'w') as o_f:
-        for correct_sentence in i_f:
-            print(correct_sentence)
-            wrong_sentence = add_noise_to_string(correct_sentence, 0.7)
-            o_f.write(wrong_sentence)
+    with open(output_mistake_filename, 'w') as o_f:
+        with open(output_correct_filename, 'w') as o_c_f:
+            for correct_sentence in i_f:
+                correct_sentence = correct_sentence[:-1]
+                print(correct_sentence)
+                wrong_sentence = add_noise_to_string(correct_sentence, 0.7)
+                o_c_f.write(correct_sentence)
+                o_f.write(wrong_sentence)
