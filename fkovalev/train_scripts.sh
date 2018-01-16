c36b9529ced18f088be1377fe5e937ee6870826c
Fedor Kovalev <fedor.kovalev@booking.com>
2018-01-16 17:23:07 +0100

    add translate run command

--- fkovalev/train_scripts.sh
+++ fkovalev/train_scripts.sh
@@ -1,9 +1,11 @@
 
 
-cat train.src.txt | sed 's/ /■/g' | sed '/\(.\)/\1 /g' > train.src.tok
+cat train.src.txt | sed 's/ /■/g' | sed 's/\(.\)/\1 /g' > train.src.tok
 
 th preprocess.lua -train_src ../opennmt_data/train.src.tok -train_tgt ../opennmt_data/train.tgt.tok -valid_src ../opennmt_data/valid.src.tok -valid_tgt ../opennmt_data/valid.tgt.tok -save_data ../opennmt_data/char_correction -src_seq_length 160 -tgt_seq_length 160
 
 
 
 CUDA_VISIBLE_DEVICES=0 th train.lua -data ../opennmt_data/char_correction-train.t7 -save_model ../opennmt_data/char_correction -learning_rate_decay 0.85 -word_vec_size 100 -rnn_size 400 -layers 3 -end_epoch 80 -encoder_type brnn -residual -max_batch_size 160 -gpuid 1 -optim adam -learning_rate 0.0002
+
+CUDA_VISIBLE_DEVICES=3 th translate.lua -model /var/data/fkovalev/char_correction_epoch4_1.03.t7 -src /var/data/fkovalev/test_data.tok -output /var/data/fkovalev/test_corrections.tok -gpuid 1
